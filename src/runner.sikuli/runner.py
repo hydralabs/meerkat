56b168d6e9da531881b73640c7fe9b7c58b2a3fa
Nick Joyce <nick@boxdesign.co.uk>
2011-05-30 17:26:11 +1000

    Check the test result

--- src/runner.sikuli/runner.py
+++ src/runner.sikuli/runner.py
@@ -1,14 +1,37 @@
 import sys
 import os
 
-print sys.argv
-print os.getcwd()
-
 flash_player = sys.argv[1]
 swf_to_play = sys.argv[2]
 
+test_timeout = 10
+
+result = 'pending'
+
 openApp(flash_player)
 switchApp(flash_player)
-type("O", KEY_SHIFT + KEY_CMD)
-paste('file://' + os.getcwd() + os.path.sep + swf_to_play)
-click(find("1306738115396.png"))
+
+def open_swf():
+	type("O", KEY_SHIFT + KEY_CMD)
+	paste('file://' + os.getcwd() + os.path.sep + swf_to_play)
+	click(find("1306738115396.png"))
+
+def onFailure(event):
+	global result
+	
+	event.region.stopObserver()
+	
+	result = 'failure'	
+
+
+
+def get_test_result():
+	onAppear("1306740001745.png", onFailure)
+	
+	observe(test_timeout)
+
+open_swf()
+get_test_result()
+closeApp(flash_player)
+
+print result
