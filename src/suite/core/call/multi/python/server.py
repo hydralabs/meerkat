f2ca6fe3df3859300558013f6cb68deead6ba433
Nick Joyce <nick@boxdesign.co.uk>
2011-07-24 11:26:08 +1000

    Implement py_server version of core_call_multi

--- src/suite/core/call/multi/python/server.py
+++ src/suite/core/call/multi/python/server.py
@@ -1,10 +1,53 @@
+import random
+
 from rtmpy import server
 
 
 
 class Client(server.Client):
-    def ping(self, callId):
+    """
+    """
+
+    def ping_server(self, callId):
         return callId
+
+
+    def start(self, repeat):
+        self.id = random.random()
+        self.callNumber = 0
+        self.repeat = repeat
+
+        from twisted.internet import reactor
+
+        reactor.callLater(0, self.sendPing)
+
+
+    def sendPing(self):
+        d = self.call('ping_client', self.id, notify=True)
+
+        def cb(result):
+            if result != self.id:
+                self.call('server_fail')
+
+                return
+
+            if self.callNumber == self.repeat:
+                self.call('server_success')
+
+                return
+
+            self.sendPing()
+
+            return result
+
+        def eb(fail):
+            self.call('server_fail')
+
+            return fail
+
+        d.addCallbacks(cb, eb)
+
+        self.callNumber += 1
 
 
 class Application(server.Application):
@@ -13,3 +56,7 @@ class Application(server.Application):
     """
 
     client = Client
+
+
+    def onConnectAccept(self, client, *args):
+        client.start(10)
